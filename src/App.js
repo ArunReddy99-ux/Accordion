ee3e7093b6a117a476ff2a5b1b9cad3a14b4b4ce
ArunReddy99-ux <arunreddyveerelly2003@gmail.com>
2024-12-03 19:21:01 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Accordion data={faqs} />
     </div>
   );
 }
 
-export default App;
+function Accordion({ data }) {
+  const [curOpen, setcurOpen] = useState(null);
+  return (
+    <div className="accordion">
+      {data.map((item, index) => (
+        <AccordionItem
+          curOpen={curOpen}
+          onOpen={setcurOpen}
+          key={index}
+          num={index + 1}
+          title={item.title}
+        >
+          {item.text}
+        </AccordionItem>
+      ))}
+      <AccordionItem
+        curOpen={curOpen}
+        onOpen={setcurOpen}
+        num={23}
+        title="TESTNG "
+      >
+        "happen to be tested before its too late "
+      </AccordionItem>
+    </div>
+  );
+}
+function AccordionItem({ num, title, curOpen, onOpen, children }) {
+  const isOpen = num === curOpen;
+  function handleClick() {
+    onOpen(isOpen ? null : num);
+  }
+  return (
+    <div
+      className={`item ${isOpen === true ? "open" : ""}`}
+      onClick={handleClick}
+    >
+      <p className="number">{num < 9 ? `0${num}` : num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen === true ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{children}</div>}
+    </div>
+  );
+}
